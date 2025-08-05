145164dc1aa4057d1a7b11afdaf5a065ad5010c7
andrei.detenkov <a.detenkov@gmail.com>
2025-08-05 16:44:32 +0600

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/eslint',
+    '@nuxt/test-utils',
+    '@nuxt/image'
+  ],
+
+  css: ['~/assets/css/main.css'],
+
+  compatibilityDate: '2025-07-16'
+})
