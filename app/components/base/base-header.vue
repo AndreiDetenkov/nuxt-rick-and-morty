9b353cade2a391a8e2344bdcc75758627f727307
andrei.detenkov <a.detenkov@gmail.com>
2025-08-05 19:08:07 +0600

    feat(layout): add BaseNav and pages for Characters and Episodes
    - Created `BaseNav` component and integrated into `BaseHeader`
    - Added pages: `Characters` and `Episodes` with basic templates
    - Updated `BaseHeader` to include `BaseNav` with navigation links

--- app/components/base/base-header.vue
+++ app/components/base/base-header.vue
@@ -3,9 +3,13 @@
 <template>
 	<header class="grid min-h-14 grid-cols-[auto_1fr_auto] items-center gap-4">
 		<div>
-			<UIcon name="i-custom-icon-logo" class="fill-primary size-10" mode="svg" />
+			<UIcon name="custom-icon:logo" class="fill-primary size-10" mode="svg" />
 		</div>
-		<div>nav</div>
+
+		<div class="flex items-center justify-end">
+			<base-nav />
+		</div>
+
 		<div>settings</div>
 	</header>
 </template>
---
+++ app/components/base/base-nav.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+export interface NavItem {
+	name: string;
+	path: string;
+}
+
+const navList: NavItem[] = [
+	{
+		name: 'Characters',
+		path: '/characters',
+	},
+	{
+		name: 'Episodes',
+		path: '/episodes',
+	},
+];
+</script>
+
+<template>
+	<nav data-test-id="nav" class="flex items-center gap-7">
+		<ULink
+			v-for="(nav, index) in navList"
+			:key="`nav-item-${index}`"
+			data-test-id="nav_link"
+			:to="nav.path"
+			raw
+			class="text-lg font-semibold transition-colors duration-300 hover:text-orange-500"
+			inactive-class="text-primary"
+			active-class="text-orange-500 underline underline-offset-4"
+		>
+			{{ nav.name }}
+		</ULink>
+	</nav>
+</template>
